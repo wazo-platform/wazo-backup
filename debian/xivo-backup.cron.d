45f0b37af19be4c7238a71daa98b5bbe4689688f
Sebastien Duthil <sduthil@wazo.community>
2020-11-23 14:51:45 -0500

    cron: create backup files after logrotate run
    Why:
    
    * Systemd runs logrotate.timer at midnight every day
    * cron runs logrotate cronjob at 06:25 every day, but does nothing for
    log files rotated daily

--- debian/xivo-backup.cron.d
+++ debian/xivo-backup.cron.d
@@ -1,3 +1,3 @@
 PATH=/usr/sbin
 
-30 6 * * *   root       xivo-backup data /var/backups/xivo/data ; xivo-backup db /var/backups/xivo/db
+5 0 * * *   root       xivo-backup data /var/backups/xivo/data ; xivo-backup db /var/backups/xivo/db
