ab361012ee5ce93e377471b1006fed1d50ed2f2c
Sebastien Duthil <sduthil@wazo.community>
2020-11-23 13:50:15 -0500

    cron: create backup right after logrotate
    also don't fail both backups if the first one fails

--- debian/xivo-backup.cron.d
+++ debian/xivo-backup.cron.d
@@ -1,3 +1,3 @@
 PATH=/usr/sbin
 
-1 0 * * *   root       xivo-backup data /var/backups/xivo/data && xivo-backup db /var/backups/xivo/db
+30 6 * * *   root       xivo-backup data /var/backups/xivo/data ; xivo-backup db /var/backups/xivo/db
